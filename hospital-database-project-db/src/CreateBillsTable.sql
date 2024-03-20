2968c77fcc5e6d839a08ee2b80c4699938067866
90sSnowfall <nicolaeciprian.dragan0424@gmail.com>
2024-03-20 20:34:27 +0200

    5 more tables
    OperationTheaters,Nurses,Watchmen,Bills and Medicines tables have been added

---
+++ hospital-database-project-db/src/CreateBillsTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Bills]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [name] CHAR(64) NOT NULL, 
+    [sum] DECIMAL NOT NULL, 
+    [state] CHAR(8) NOT NULL, 
+    [patient_id] INT NOT NULL
+)
---
+++ hospital-database-project-db/src/CreateOperationTheatersTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[OperationTheaters]
+(
+	[number] INT NOT NULL PRIMARY KEY, 
+    [floor] INT NOT NULL, 
+    [specialization] VARCHAR(64) NOT NULL
+)
