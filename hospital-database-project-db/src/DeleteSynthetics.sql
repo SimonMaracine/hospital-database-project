e8a87c0fc0f7a7b56b74a363001bb340377ff832
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2024-03-28 18:36:03 +0200

    Updated so it now deletes all entries from all tables

--- hospital-database-project-db/src/DeleteSynthetics.sql
+++ hospital-database-project-db/src/DeleteSynthetics.sql
@@ -1,7 +1,50 @@
 USE [hospital-database-project-db]
 GO
 
+DELETE FROM dbo.DoctorsPartners;
+GO
+
+DELETE FROM dbo.TreatmentsMedicines;
+GO
+
+DELETE FROM dbo.Partners;
+GO
+
+DELETE FROM dbo.Apprentices;
+GO
+
+DELETE FROM dbo.Bills;
+GO
+
+DELETE FROM dbo.Patients;
+GO
+
 DELETE FROM dbo.Watchmen;
 GO
 
+DELETE FROM dbo.Nurses;
+GO
 
+DELETE FROM dbo.OperationTheaters;
+GO
+
+DELETE FROM dbo.ICUs;
+GO
+
+DELETE FROM dbo.Treatments;
+GO
+
+DELETE FROM dbo.Doctors;
+GO
+
+DELETE FROM dbo.Employees;
+GO
+
+DELETE FROM dbo.Shifts;
+GO
+
+DELETE FROM dbo.Medicines;
+GO
+
+DELETE FROM dbo.Rooms;
+GO
