a83b9fdb47a8004e41dd80d6a04b517c403cb96d
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2024-03-30 12:03:09 +0200

    Forgot to add auto_increment to rooms id

--- hospital-database-project-db/src/CreateDatabase.sql
+++ hospital-database-project-db/src/CreateDatabase.sql
@@ -48,7 +48,7 @@ CREATE TABLE [dbo].[Medicines]
 
 CREATE TABLE [dbo].[Rooms]
 (
-    [id] INT NOT NULL PRIMARY KEY,
+    [id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [type] CHAR(64) NOT NULL
 );
 
