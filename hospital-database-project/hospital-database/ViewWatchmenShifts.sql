dd2e66880c8c831e75348004fc4f4fc33c246505
90sSnowfall <nicolaeciprian.dragan0424@gmail.com>
2024-04-03 12:37:19 +0300

    Added views in the project (properly this time)

---
+++ hospital-database-project/hospital-database/ViewWatchmenShifts.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW ViewWatchmenShifts AS
+SELECT 
+    w.id AS WatchmanID,
+    CONCAT(w.first_name, ' ', w.last_name) AS WatchmanName,
+    s.description AS ShiftDescription
+FROM Watchmen w
+INNER JOIN Employees e ON w.id = e.id
+INNER JOIN Shifts s ON e.shift_id = s.id;
+GO
