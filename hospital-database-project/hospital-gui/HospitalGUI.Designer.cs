2b14e93f2bf2f717046b7de543b0a77be0916265
Simon Maracine <simonmaracine@gmail.com>
2024-04-09 23:25:09 +0300

    Connect to the database

--- hospital-database-project/hospital-gui/HospitalGUI.Designer.cs
+++ hospital-database-project/hospital-gui/HospitalGUI.Designer.cs
@@ -28,21 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectMenuItem,
+            this.disconnectMenuItem,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // connectMenuItem
+            // 
+            this.connectMenuItem.Name = "connectMenuItem";
+            this.connectMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.connectMenuItem.Text = "Connect";
+            this.connectMenuItem.Click += new System.EventHandler(this.connectMenuItem_Click);
+            // 
+            // disconnectMenuItem
+            // 
+            this.disconnectMenuItem.Name = "disconnectMenuItem";
+            this.disconnectMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.disconnectMenuItem.Text = "Disconnect";
+            this.disconnectMenuItem.Click += new System.EventHandler(this.disconnectMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
             // 
             // HospitalGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "HospitalGUI";
             this.Text = "Hospital GUI";
             this.Load += new System.EventHandler(this.HospitalGUI_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
     }
 }
 
--- hospital-database-project/hospital-gui/HospitalGUI.cs
+++ hospital-database-project/hospital-gui/HospitalGUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace hospital_gui
 {
     public partial class HospitalGUI : Form
     {
+        private readonly string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=hospital-database;Integrated Security=True;Pooling=False;Connect Timeout=30";
+        private SqlConnection connection;
+
         public HospitalGUI()
         {
             InitializeComponent();
@@ -20,6 +24,60 @@ namespace hospital_gui
         private void HospitalGUI_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void connectMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection != null)
+            {
+                MessageBox.Show("Already an existing connection");
+                return;
+            }
+
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Could not open connection: " + ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Could not open connection: " + ex.Message);
+            }
+        }
+
+        private void disconnectMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection == null)
+            {
+                MessageBox.Show("No connection");
+                return;
+            }
+
+            try
+            {
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error closing connection: " + ex.Message);
+            }
+
+            connection = null;
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+
+            Application.Exit();
         }
     }
 }
